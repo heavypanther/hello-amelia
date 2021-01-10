1ef2d7f5c4f3c74d13c87b8dacadde2fb6beb5b5
heavypanther <github@heavypanther.com>
2021-01-10 18:36:44 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+basic.clear_screen()
+basic.show_string("Hi Amelia")
+soundExpression.giggle.play()
+basic.show_icon(IconNames.HEART)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.clearScreen()
+basic.showString("Hi Amelia")
+soundExpression.giggle.play()
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
